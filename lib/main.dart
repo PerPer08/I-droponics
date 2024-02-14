483476860b5c68bb139ee044dd8ca0b4bcd080fb
Kent Lorenz Daria <kentlorenz.daria@wvsu.edu.ph>
2024-02-14 11:36:36 +0800

    design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'dart:convert';
 
@@ -54,32 +56,144 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('I-droponics'),
-        ),
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                height: 200,
-                width: 200,
-                padding: EdgeInsets.all(20),
-                child: Container(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
                   height: 200,
-                  width: 100,
-                  child: Column(children: [
-                    Text("ph level: $ph"),
-                    const Divider(),
-                    Text("tds value: $tds"),
-                    const Divider(),
-                    Text("temperature: $temp"),
-                    const Divider(),
-                  ]),
+                  width: 200  ,
+                  child: Image.asset('assets/Logo.png', ),
                 ),
-              ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Card(
+                          child: Container(
+                              decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xff085078),
+                                      Color(0xff85d8ce)
+                                    ],
+                                    stops: [0, 1],
+                                    begin: Alignment.bottomRight,
+                                    end: Alignment.topLeft,
+                                  )),
+                              width: 150,
+                              height: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 10.0),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const Text("pH",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15)),
+                                      Text(
+                                        "$ph",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ]),
+                              ))),
+                      Card(
+                          child: Container(
+                              decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xff085078),
+                                      Color(0xff85d8ce)
+                                    ],
+                                    stops: [0, 1],
+                                    begin: Alignment.bottomRight,
+                                    end: Alignment.topLeft,
+                                  )),
+                              width: 150,
+                              height: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 10.0),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const Text("TDS",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15)),
+                                      Text(
+                                        "$tds",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ]),
+                              ))),
+                    ]),
+                Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Card(
+                          child: Container(
+                              decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xff085078),
+                                      Color(0xff85d8ce)
+                                    ],
+                                    stops: [0, 1],
+                                    begin: Alignment.bottomRight,
+                                    end: Alignment.topLeft,
+                                  )),
+                              width: 150,
+                              height: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0, vertical: 10.0),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const Text("Temperature",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15)),
+                                      Text(
+                                        "$temp",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ]),
+                              ))),
+                    ]),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
